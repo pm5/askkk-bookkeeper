f365be2bf6b46e17735bd268836b7e1c6a695ada
Pomin Wu <pomin5@gmail.com>
2014-05-14 12:11:26 +0800

    add code to calculate total questions

---
+++ app.js
@@ -0,0 +1,37 @@
+var nconf = require('nconf');
+nconf.file({ file: 'config/askkk-bookkeeper.json' })
+  .defaults({
+    "firebase": "https://askkkkk-dev.firebaseio.com/",
+    "firebase_secret": "etZEKPiPhvkc7kKBoyAQDCe8vEr0ykp5nO5cMlgZ"
+  });
+
+var root = new (require('firebase'))(nconf.get('firebase'));
+root.auth(nconf.get('firebase_secret'), function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
+
+root.child('questions_count').set(0, function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
+root.child('questions').on('child_added', function (snapshot) {
+  root.child('questions_count').transaction(function (it) {
+    return it + 1;
+  }, function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
+root.child('questions').on('child_removed', function (snapshot) {
+  root.child('questions_count').transaction(function (it) {
+    return it - 1;
+  }, function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
