360b7e545580ed7ec5390e1bbfd84b7a93895000
Pomin Wu <pomin5@gmail.com>
2014-05-30 10:41:34 +0800

    extract askkk module

--- bookkeeper.js
+++ bookkeeper.js
@@ -1,16 +1,4 @@
-var nconf = require('nconf');
+var askkk = require('./lib/askkk');
 var question = require('./lib/question');
-nconf.file({ file: 'config/askkk-bookkeeper.json' })
-  .defaults({
-    "firebase": "https://askkkkk-dev.firebaseio.com/",
-    "firebase_secret": "etZEKPiPhvkc7kKBoyAQDCe8vEr0ykp5nO5cMlgZ"
-  });
 
-var root = new (require('firebase'))(nconf.get('firebase'));
-root.auth(nconf.get('firebase_secret'), function (error) {
-  if (error) {
-    console.log(error);
-  }
-});
-
-question.monitor(root);
+question.monitor(askkk.root);
