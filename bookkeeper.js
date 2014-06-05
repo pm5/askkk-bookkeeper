867e43b27594388e84637a5f433266fd167ecbdf
Pomin Wu <pomin5@gmail.com>
2014-06-05 15:06:03 +0800

    add category groups

--- bookkeeper.js
+++ bookkeeper.js
@@ -2,5 +2,5 @@ var askkk = require('./lib/askkk');
 var question = require('./lib/question');
 var user = require('./lib/user');
 
-question.monitor(askkk.root);
-user.monitor(askkk.root);
+question.monitor(askkk);
+user.monitor(askkk);
