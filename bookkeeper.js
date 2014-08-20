b834fa8e60f4aa971d171e06f29546e532b6be73
soidid <soidid@gmail.com>
2014-08-20 18:32:22 +0800

    change to 'monitor' format. specify testing json data for each signature test.

--- bookkeeper.js
+++ bookkeeper.js
@@ -1,8 +1,8 @@
 var askkk = require('./lib/askkk');
 var question = require('./lib/question');
 var user = require('./lib/user');
-var user = require('./lib/signature');
+var signature = require('./lib/signature');
 
-question.monitor(askkk);
-user.monitor(askkk);
-signature.monitor(askkk);
+//question.monitor(askkk);
+//user.monitor(askkk);
+//signature.monitor(askkk);
--- test/signature.js
+++ test/signature.js
@@ -6,15 +6,15 @@ var default_timeout = 10000;
 var root = askkk.root;
 var questionID = '-JGJj9MVjeU7ezA_XC5R';
 
-describe('Signatures count', function () {
+describe('Signatures Count', function () {
   this.timeout(default_timeout);
   beforeEach(function (done) {//before every test.
 
-    fs.readFile('test/signatures.json', function (error, data) {
+    fs.readFile('test/signature/signatures.json', function (error, data) {
       expect(error).to.be(null);
       root.child('signatures').set(JSON.parse(data), function () {
         /* ----- */
-        fs.readFile('test/questions.json', function (error, data) {
+        fs.readFile('test/signature/questions.json', function (error, data) {
           expect(error).to.be(null);
           root.child('questions').set(JSON.parse(data), function () {
             setTimeout(function () {
@@ -64,6 +64,19 @@ describe('Signatures count', function () {
         }, default_wait);
       });
     });
+  });
+
+  it('should re-calculate the number when question.signatures_count changed.', function (done) {
+    var ref = root.child('questions/'+ questionID + '/signatures_count').set(
+      2014,
+    function (error) {
+        setTimeout(function () {
+          root.child('questions/'+ questionID + '/signatures_count').once('value', function (snapshot) {
+            expect(snapshot.val()).to.be(1509);
+            done();
+          });
+        }, default_wait);
+      });
   });
 
 
