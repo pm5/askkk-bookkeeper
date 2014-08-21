f83926c9a305d4963e9d74334011840ea6bf8afd
soidid <soidid@gmail.com>
2014-08-21 15:28:34 +0800

    fix testing data no collecting q.

--- bookkeeper.js
+++ bookkeeper.js
@@ -2,7 +2,9 @@ var askkk = require('./lib/askkk');
 var question = require('./lib/question');
 var user = require('./lib/user');
 var signature = require('./lib/signature');
+var priority = require('./lib/priority');
 
 //question.monitor(askkk);
 //user.monitor(askkk);
 //signature.monitor(askkk);
+//priority.monitor(askkk);
